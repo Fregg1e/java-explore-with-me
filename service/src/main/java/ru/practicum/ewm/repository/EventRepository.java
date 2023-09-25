80df0fc10b678abef05faa8040fd4cd742649e4a
Alexandr Shalaginov <alxshlgnv@yandex.ru>
2023-09-25 12:13:45 +0300

    fix: исправить запросы в EventRepo

--- service/src/main/java/ru/practicum/ewm/repository/EventRepository.java
+++ service/src/main/java/ru/practicum/ewm/repository/EventRepository.java
@@ -17,15 +17,16 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> getEventsByUserId(Long userId, Pageable pageable);
 
     @Query("SELECT e FROM Event e WHERE ((e.initiator.id IN ?1) OR ?1 IS NULL) AND ((e.state IN ?2) OR ?2 IS NULL) "
-            + "AND ((e.category.id IN ?3) OR ?3 IS NULL) AND ((e.eventDate >= ?4)  OR ?4 IS NULL) "
-            + "AND ((e.eventDate <= ?5)  OR ?5 IS NULL)")
+            + "AND ((e.category.id IN ?3) OR ?3 IS NULL) AND ((e.eventDate >= ?4) OR cast(?4 as timestamp) IS NULL) "
+            + "AND ((e.eventDate <= ?5) OR cast(?5 as timestamp) IS NULL)")
     List<Event> getEventsAdmin(List<Long> users, List<EventState> states, List<Long> categories,
             LocalDateTime rangeStart, LocalDateTime rangeEnd, Pageable pageable);
 
     @Query("SELECT e FROM Event e "
             + "WHERE ((UPPER(e.annotation) LIKE UPPER(?1)) OR (UPPER(e.description) LIKE UPPER(?1)) OR ?1 IS NULL) "
             + "AND ((e.category.id IN ?2) OR ?2 IS NULL) AND ((e.paid = ?3) OR ?3 IS NULL) "
-            + "AND ((e.eventDate >= ?4)  OR ?4 IS NULL) AND ((e.eventDate <= ?5)  OR ?5 IS NULL) "
+            + "AND ((e.eventDate >= ?4) OR cast(?4 as timestamp) IS NULL) "
+            + "AND ((e.eventDate <= ?5) OR cast(?5 as timestamp) IS NULL) "
             + "AND e.state = 'PUBLISHED'")
     List<Event> getEvents(String text, List<Long> categories, Boolean paid,
             LocalDateTime rangeStart, LocalDateTime rangeEnd, Pageable pageable);
